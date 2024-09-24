673b58529104113a9053176efe2d481565785210
Zander Coetzee <coettools@gmail.com>
2024-09-24 11:52:19 -0700

    initial push

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../src/jsx.d.ts" />
+import { Component, memo } from "./Component";
+import { createElement, CTFramework } from "./CTFramework";
+import { VNode } from "./types";
+export type { VNode };
+export { Component, memo, createElement };
+export default CTFramework;
