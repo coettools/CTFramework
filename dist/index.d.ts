5254bcdc1bbef554f10da38b8328d6e69d91510e
Zander Coetzee <coettools@gmail.com>
2024-09-24 20:40:25 -0700

    cleanup

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../src/jsx.d.ts" />
-import { Component, memo } from "./Component";
-import { createElement, CTFramework } from "./CTFramework";
-import { VNode } from "./types";
-export type { VNode };
-export { Component, memo, createElement };
-export default CTFramework;
